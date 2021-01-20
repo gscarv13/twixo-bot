e24edd987b52eb0d73f7bb022f145dd5fb5dd669
Gustavo Carvalho <gsc13@tuta.io>
2021-01-20 18:15:09 -0300

    Add basic logic to bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,2 +1,43 @@
 require 'telegram/bot'
 require_relative 'source'
+
+class Bot
+  @token = '1446566008:AAGmBzXPVGFvOmxoy146geEsZM9DO-IhqGI'
+
+  Telegram::Bot::Client.run(@token) do |bot|
+    bot.listen do |message|
+      case messege.text
+      when '/start'
+        bot.api.send_message(
+          chat_id: message.chat.id, text: "Welcome, #{message.from.first_name}! I am Twixo the news bot.
+           I will bring the top 7 articles on four different categories, /bitcoin, /tech, /wallstreet or
+           /business. To begin /start to run the bot, and choose one of the options.
+            To stop, please run /stop at any time."
+        )
+
+      when '/stop'
+        bot.api.send_message(
+          chat_id: message.chat.id, text: "See you soon, #{message.from.first_name}", date: message.date
+        )
+
+      when '/bitcoin'
+        bot.api.send_message(chat_id: message.chat.id, text: 'bitcoin works', date: message.date)
+
+      when '/tech'
+        bot.api.send_message(chat_id: message.chat.id, text: 'tech works', date: message.date)
+
+      when '/business'
+        bot.api.send_message(chat_id: message.chat.id, text: 'business works', date: message.date)
+
+      when '/wallstreet'
+        bot.api.send_message(chat_id: message.chat.id, text: 'wallstreet works', date: message.date)
+
+      else
+        bot.api.send_message(
+          chat_id: message.chat.id, text: "I haven't learned that yet hehe.
+          Please enter one of the following: /start,  /stop , /bitcoin, tech, /wallstreet or /business"
+        )
+      end
+    end
+  end
+end
