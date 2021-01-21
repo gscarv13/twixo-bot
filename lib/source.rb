114ad698b8481faa7d89be0abed7eaab1b714bc9
Gustavo Carvalho <gsc13@tuta.io>
2021-01-21 10:18:49 -0300

    Require dotenv, add API to env file

--- lib/source.rb
+++ lib/source.rb
@@ -1,4 +1,17 @@
 require 'telegram/bot'
 require 'net/http'
 require 'json'
-require_relative 'bot'
+require 'dotenv'
+# require_relative 'bot'
+
+Dotenv.load('token_source.env')
+
+class Source
+  attr_reader :listing
+
+  NEWS_API = ENV['TOKEN_SOURCE']
+
+  def initialize
+    @listing = nil
+  end
+end
