9b455ac9eb170dec9867b53d0c362909323f7f04
Gustavo Carvalho <gsc13@tuta.io>
2021-01-22 20:48:25 -0300

    Add tests to News class

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -8,14 +8,32 @@ describe News do
   let(:news) { News.new }
 
   describe '#output' do
-    it 'Raise ArgumentError in no input is passed in' do
+    it 'Raise ArgumentError if no input is passed in' do
       expect { news.output }.to raise_exception ArgumentError
     end
     it 'Invalid input should return nil' do
       expect(news.output('my-news-website')).to be nil
     end
-    it 'Length should be 1000 or more' do
-      expect(news.output('wired').length).to be >= 1000
+    it 'Invalid input should return nil' do
+      expect(news.output('3215487942513')).to be nil
+    end
+    it 'Invalid input should return nil' do
+      expect(news.output('aomnetiolcn&ig%30ds821f-djh942513')).to be nil
+    end
+    it 'Valid source should not be nil' do
+      expect(news.output('wired')).to be_truthy
+    end
+    it 'Valid source should not be nil' do
+      expect(news.output('the-verge')).to be_truthy
+    end
+    it 'Valid source should not be nil' do
+      expect(news.output('techcrunch')).to be_truthy
+    end
+    it 'Valid source should not be nil' do
+      expect(news.output('techradar')).to be_truthy
+    end
+    it 'Valid source should not be nil' do
+      expect(news.output('the-next-web')).to be_truthy
     end
   end
 end
